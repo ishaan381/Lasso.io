b4a00b06ddbeccd2de259a224ff372c2399c835b
Jonathan Ligh <JonathankLigh@gmail.com>
2016-08-10 16:12:43 -0400

    basic routes done

--- server/app/routes/companies/index.js
+++ server/app/routes/companies/index.js
@@ -2,21 +2,29 @@
 var router = require('express').Router();
 module.exports = router;
 let Company = require('../../../db/models/company'),
+Job_applications = require('../../../db/models/job.application'),
+Job_descriptions = require('../../../db/models/job.description'),
 check = require('../check-handler');
 
 router.param('id', function(req, res, next, id){
 	Company.findOne({
 		where:{
 			id: id
-		},
-		//include: [] for later
+		}, //we'll figure this out later
+		include: [
+		{ model: Job, as: 'jobs'},
+		{ model: User, as: 'employee'},
+		{ model: Job_applications, as: 'appForm'},
+		{ model: Job_descriptions, as: 'jobDesc'}
+		]
 	})
-	.then(function(user){
+	.then(function(company){
 		req.requestedCompany = company;
 		next();
 	})
 	.catch(next);
 });
+
 //what would the admin route look like
 router.get('/:id', check.company, function(req, res, next) {
 	req.requestedCompany.reload()
@@ -33,3 +41,11 @@ router.post('/', function(req, res, next) {
 	})
 	.catch(next);
 });
+
+router.put('/:id', function(req, res, next) {
+	req.requestedCompany.update(req.body)
+	.then(function (user) {
+		res.send(user);
+	})
+	.catch(next);
+})
---
+++ server/app/routes/jobs/index.js
@@ -0,0 +1,52 @@
+'use strict';
+var router = require('express').Router();
+module.exports = router;
+
+let Job = require('../../../db/models/job'),
+check = require('../check-handler');
+
+router.param('id', function(req, res, next, id){
+	Job.findOne({
+		where:{
+			id: id
+		}
+	})
+	.then(function(job){
+		req.requestedJob = job;
+		next();
+	})
+	.catch(next);
+});
+
+router.get('/:id', function(req, res, next) {
+    req.requestedJob.reload()
+    .then(function(desc) {
+        res.send(desc)
+    })
+    .catch(next);
+});
+
+router.post('/', function(req, res, next) {
+	Job.create(req.body)
+	.then(function(desc){
+		res.send(desc);
+	})
+	.catch(next);
+});
+
+router.put('/:id', check.access, function(req, res, next) {
+    req.requestedJob.update(req.body)
+    .then(function (desc) {
+        res.send(desc);
+    })
+    .catch(next);
+});
+
+router.delete('/:id', check.company, function(req, res, next) {
+    req.requestedJob.destroy()
+    .then(function () {
+        res.status(204).end();
+    })
+    .catch(next);
+});
+
