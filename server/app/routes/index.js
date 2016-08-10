7627ae217bae43f5edde27ed0ffcefffad7fc870
Jonathan Ligh <JonathankLigh@gmail.com>
2016-08-10 15:20:01 -0400

    made a lot of route files

--- server/app/routes/index.js
+++ server/app/routes/index.js
@@ -1,9 +1,13 @@
 'use strict';
 var router = require('express').Router();
 module.exports = router;
-
+//all routes are plural
 router.use('/members', require('./members'));
-
+router.use('/companies', require('./companies'));
+router.use('/job.applications', require('./job.applications'));
+router.use('/job.descriptions', require('./job.descriptions'));
+router.use('/jobs', require('./jobs'));
+router.use('/users', require('./users'));
 // Make sure this is after all of
 // the registered routes!
 router.use(function (req, res) {
