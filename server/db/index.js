ed607e09563a7b09b84761ef16d8aac4ab3ce827
Payton Henson <pph6@georgetown.edu>
2016-08-10 14:39:40 -0400

    first associations done

--- server/db/index.js
+++ server/db/index.js
@@ -3,4 +3,21 @@ var db = require('./_db');
 module.exports = db;
 
 var User = require('./models/user');
+var Job = require('./models/job');
+var JobDescription = require('./models/job.description');
+var JobApplication = require('./models/job.application');
+var Company = require('./models/company');
+
+//places descriptionId and applicationId on the Job table
+JobDescription.hasOne(Job, {foreignKey: 'descriptionId'});
+JobApplication.hasOne(Job, {foreignKey: 'applicationId'});
+
+Job.belongsTo(JobDescription, {as: 'description'});
+Job.belongsTo(JobApplication, {as: 'application'});
+
+Company.hasMany(User);
+User.belongsTo(Company);
+
+
+
 
