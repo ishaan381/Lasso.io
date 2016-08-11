888eec9bc6dd9a32d27ac71cf6bc51b897fd8b5f
Kevin Li <kl2482@nyu.edu>
2016-08-11 15:52:24 -0400

    more description work

--- browser/js/postings/description/postings.description.controller.js
+++ browser/js/postings/description/postings.description.controller.js
@@ -88,21 +88,37 @@ app.controller('newDescriptionCtrl', function ($scope, formlyVersion, $q, $http,
       },
       {
         key: 'region',
-        type: 'ui-select-single-select2',
+        type: 'ui-select-single-search',
         className: 'region-field col-md-4',
         templateOptions: {
           optionsAttr: 'bs-options',
           ngOptions: 'option[to.valueProp] as option in to.options | filter: $select.search',
-          label: 'State/Region',
-          valueProp: 'id',
-          labelProp: 'label',
-          placeholder: 'State/Region',
-          options: testData
+          label: 'Region',
+          valueProp: 'formatted_address',
+          labelProp: 'formatted_address',
+          placeholder: 'Search',
+          options: [],
+          refresh: refreshAddresses,
+          refreshDelay: 0
         }
       },
+      // {
+      //   key: 'region',
+      //   type: 'ui-select-single-select2',
+      //   className: 'region-field col-md-4',
+      //   templateOptions: {
+      //     optionsAttr: 'bs-options',
+      //     ngOptions: 'option[to.valueProp] as option in to.options | filter: $select.search',
+      //     label: 'State/Region',
+      //     valueProp: 'id',
+      //     labelProp: 'label',
+      //     placeholder: 'State/Region',
+      //     options: testData
+      //   }
+      // },
           // NOT WORKING YET
     {
-      key: 'commitment', 
+      key: 'current', 
       type: 'toggleCheckbox',
       className: 'col-md-4',
       templateOptions: {
@@ -111,12 +127,55 @@ app.controller('newDescriptionCtrl', function ($scope, formlyVersion, $q, $http,
       }
     },
       {
-        key: 'myText',
+        key: 'description',
         type: 'textEditor',
+        className: 'text-editor',
         templateOptions: {
-          label: 'Enter Text'
+          label: 'Job Description'
         }
       },
+      {
+        noFormControl: true,
+        template: '<h3>SECTIONS (for requirements, responsibilities, etc.)</h3><hr>'
+      },
+      {
+    "type": "repeatSection",
+    "key": "sections",
+    "templateOptions": {
+      "btnText": "Add another section",
+      "fields": [
+        {
+          "className": "row",
+          "fieldGroup": [
+            {
+              "className": "col-md-12 section-title-field",
+              "type": "input",
+              "key": "sectionTitle",
+              "templateOptions": {
+                "label": "Section Title",
+              }
+            }
+          ]
+        },
+        {
+
+          "className": "row",
+          "fieldGroup": [
+            {
+              "key": 'description',
+              "type": 'textEditor',
+              "className": 'text-editor col-md-12',
+              "templateOptions": {
+                "label": 'Section Body'
+              }
+            },
+
+          ]
+        }
+
+      ]
+    }
+  },
       {
         key: 'multipleOption',
         type: 'ui-select-multiple',
@@ -130,35 +189,26 @@ app.controller('newDescriptionCtrl', function ($scope, formlyVersion, $q, $http,
           options: testData
         }
       },
-      {
-        key: 'singleOptionAsync',
-        type: 'ui-select-single-search',
-        templateOptions: {
-          optionsAttr: 'bs-options',
-          ngOptions: 'option[to.valueProp] as option in to.options | filter: $select.search',
-          label: 'Async Search',
-          valueProp: 'formatted_address',
-          labelProp: 'formatted_address',
-          placeholder: 'Search',
-          options: [],
-          refresh: refreshAddresses,
-          refreshDelay: 0
-        }
-      },
     ];
 
     vm.originalFields = angular.copy(vm.fields);
     
     function refreshAddresses(address, field) {
+      console.log(address);
+      console.log('hi');
       var promise;
       if (!address) {
         promise = $q.when({data: {results: []}});
       } else {
-        var params = {address: address, sensor: false};
-        var endpoint = '//maps.googleapis.com/maps/api/geocode/json';
-        promise = $http.get(endpoint, {params: params});
+        // var params = {address: address, components: "country: vm.model.country", sensor: false};
+        // console.log(params);
+        // var endpoint = '//maps.googleapis.com/maps/api/geocode/json';
+        var endpoint = '//maps.googleapis.com/maps/api/geocode/json?components=administrative_area:' + address + '|country:' + vm.model.country
+        // promise = $http.get(endpoint, {params: params});
+        promise = $http.get(endpoint);
       }
       return promise.then(function(response) {
+        console.log(response.data.results);
         field.templateOptions.options = response.data.results;
       });
     }
