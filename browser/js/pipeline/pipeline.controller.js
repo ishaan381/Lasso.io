ba6d13a046a564dbb00cacc6bf339c8ceacb6314
Payton Henson <gator293@gmail.com>
2016-08-17 12:47:33 -0400

    pipeline comes from jobId

--- browser/js/pipeline/pipeline.controller.js
+++ browser/js/pipeline/pipeline.controller.js
@@ -2,6 +2,7 @@ app.controller('pipelineCtrl', function ($scope, Pipeline, $stateParams) {
 
   Pipeline.getStages($stateParams.jobId)
   .then(stages => {
+    console.log(stages);
     $scope.stages = stages;
   });
 
--- browser/js/pipeline/pipeline.state.js
+++ browser/js/pipeline/pipeline.state.js
@@ -1,6 +1,6 @@
 app.config($stateProvider => {
   $stateProvider.state('pipeline', {
-    url: '/hire/postings/:id/pipeline',
+    url: '/hire/postings/:jobId/pipeline',
     templateUrl: '/js/pipeline/pipeline.html',
     controller: 'pipelineCtrl'
   });
