b5f76d98ce98a043c01d4d3280d5721828d4c574
Payton Henson <gator293@gmail.com>
2016-08-19 15:07:38 -0400

    full merge

--- browser/js/applicant-form/applicant-form.controller.js
+++ browser/js/applicant-form/applicant-form.controller.js
@@ -1,21 +1,11 @@
-<<<<<<< HEAD
+
 app.controller('applicantFormCtrl', function($rootScope, $scope, formlyVersion, $stateParams, job, App, $state, JobApplication) {
-=======
-app.controller('applicantFormCtrl', function(_, $rootScope, $scope, formlyVersion, $q, $http, $stateParams, job, App, $state, JobApplication, Upload, $timeout) {
->>>>>>> master
 
-    $rootScope.$broadcast('applicantView');
+  $rootScope.$broadcast('applicantView');
 
-<<<<<<< HEAD
-  console.log(job);
-
-  $scope.description = JSON.parse(job.jobDescription.fields);
-  let application = JSON.parse(job.jobApplication.fields);
-=======
-    console.log(job)
     $scope.description = JSON.parse(job.jobDescription.fields);
     let application = JSON.parse(job.jobApplication.fields);
->>>>>>> master
+
 
     $scope.onSubmit = function(model) {
         App.create($stateParams.jobId, model)
@@ -37,39 +27,39 @@ app.controller('applicantFormCtrl', function(_, $rootScope, $scope, formlyVersio
     $scope.fields = JobApplication.getFields(application);
 
 
-    $scope.$watch('file', function() {
-        if ($scope.file != null) {
-            $scope.upload($scope.file);
-        }
-    });
+    // $scope.$watch('file', function() {
+    //     if ($scope.file != null) {
+    //         $scope.upload($scope.file);
+    //     }
+    // });
 
-    $scope.upload = function(file) {
+    // $scope.upload = function(file) {
 
-        if (!file.$error) {
-            Upload.upload({
-                url: '/api/upload',
-                data: {
-                    username: $scope.username,
-                    file: file
-                }
-            }).then(function(resp) {
-                console.log('SUCCESS!', resp)
-                $timeout(function() {
-                    $scope.log = 'file: ' +
-                        resp.config.data.file.name +
-                        ', Response: ' + JSON.stringify(resp.data) +
-                        '\n' + $scope.log;
-                });
-            }, null, function(evt) {
-                var progressPercentage = parseInt(100.0 *
-                    evt.loaded / evt.total);
-                $scope.log = 'progress: ' + progressPercentage +
-                    '% ' + evt.config.data.file.name + '\n' +
-                    $scope.log;
-                    console.log($scope.log)
-            });
-        }
-    };
+    //     if (!file.$error) {
+    //         Upload.upload({
+    //             url: '/api/upload',
+    //             data: {
+    //                 username: $scope.username,
+    //                 file: file
+    //             }
+    //         }).then(function(resp) {
+    //             console.log('SUCCESS!', resp)
+    //             $timeout(function() {
+    //                 $scope.log = 'file: ' +
+    //                     resp.config.data.file.name +
+    //                     ', Response: ' + JSON.stringify(resp.data) +
+    //                     '\n' + $scope.log;
+    //             });
+    //         }, null, function(evt) {
+    //             var progressPercentage = parseInt(100.0 *
+    //                 evt.loaded / evt.total);
+    //             $scope.log = 'progress: ' + progressPercentage +
+    //                 '% ' + evt.config.data.file.name + '\n' +
+    //                 $scope.log;
+    //                 console.log($scope.log)
+    //         });
+    //     }
+    // };
 
 
 });
