1db2f7853383757bf0a2641e47743c2f76d40266
Kevin Li <kl2482@nyu.edu>
2016-08-20 19:21:36 -0400

    wow

--- browser/js/pipeline/pipeline.stage/pipeline.stage.candidate/pipeline.stage.candidate.controller.js
+++ browser/js/pipeline/pipeline.stage/pipeline.stage.candidate/pipeline.stage.candidate.controller.js
@@ -1,9 +1,47 @@
-app.controller('candidateCtrl', function ($scope, $state, $stateParams, $timeout, stage) {
+app.controller('candidateCtrl', function($scope, $state, $stateParams, $timeout, stage, candidate, thisJob) {
 
-	$scope.currentStage = stage;
-	console.log("STAGE BOO BOO", stage)
-  $timeout(function () {
-  	$scope.currentStage.title = "Hi";
-  }, 5000)
+	console.log(candidate);
+    $scope.candidate = candidate;
+    $scope.currentStage = stage;
+    $scope.currTabId = 0;
+    $scope.currPanelTemplate;
+
+    const templateRoot = "js/pipeline/pipeline.stage/pipeline.stage.candidate/panelTemplates/";
+
+    $scope.switchTab = switchTab;
+
+    $scope.data;
+
+
+    // note panel id is += 2 ...
+    function switchTab(tabId) {
+        $scope.currTabId = tabId;
+        // tab is a panel
+        if (tabId === 0) {
+        	console.log(candidate)
+            $scope.data = { comments: candidate.comments }
+            $scope.currPanelTemplate = templateRoot + 'comments.html';
+        } else if (tabId === 1) {
+            $scope.data = { app: candidate.application, appQuestions: thisJob.jobApplication }
+            $scope.currPanelTemplate = templateRoot + 'application.html';
+            
+        } else if (tabId > 1) {
+            var currentPanel = $scope.currentStage.panels[tabId - 2];
+            if (currentPanel.hasOwnProperty("panelQuestions")) {
+                $scope.data = { questions: currentPanel.panelQuestions }
+                $scope.currPanelTemplate = templateRoot + 'kit.html'
+            } else if (currentPanel.hasOwnProperty('panelNotes')) {
+                $scope.data = {notes: currentPanel.panelNotes}
+                $scope.currPanelTemplate = templateRoot + 'notes.html'
+                
+
+            }
+        }
+    }
+
+    switchTab(0);
+
+
+    console.log("STAGE BOO BOO", stage)
 
 });
