dd2e5df385597b141306b8724c5560106743b517
Kevin Li <kl2482@nyu.edu>
2016-08-20 15:47:24 -0400

    hi

--- browser/js/pipeline/pipeline.stage/pipeline.stage.candidate/pipeline.stage.candidate.state.js
+++ browser/js/pipeline/pipeline.stage/pipeline.stage.candidate/pipeline.stage.candidate.state.js
@@ -1,6 +1,6 @@
 app.config($stateProvider => {
   $stateProvider.state('pipeline.stage.candidate', {
-    url: '/candidate/:candidateId?test',
+    url: '/candidate/:candidateId',
     templateUrl: '/js/pipeline/pipeline.stage/pipeline.stage.candidate/pipeline.stage.candidate.html',
     controller: 'candidateCtrl'
   });
