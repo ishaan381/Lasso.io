634441cab63cd84ae02a3ff1f66443b2835321e9
ishaan381 <in2187@columbia.edu>
2016-08-23 17:53:31 -0400

    fixed bower components

--- bower_components/angular-formly-repeating-section/src/index.js
+++
@@ -1,8 +0,0 @@
-const ngModuleName = 'formlyRepeatingSection';
-const angular = window.angular;
-const ngModule = angular.module(ngModuleName, [ 'formly' ]);
-
-import repeatingSection from './repeating-section';
-repeatingSection(ngModule);
-
-export default ngModuleName;
